4a1d491650871e02ebd0c8c624bce1a18b5ea505
benjamin <benjaminoheneasare65@gmail.com>
2025-01-30 19:33:17 -0800

    changes

--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -10,6 +10,13 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('about', views.about, name='about'),
+    path('contact', views.contact, name='contact'),
+    path('donate', views.donate, name='donate'),
+    path('error', views.error, name='error'),
+    path('myservice', views.myservice, name='myservice'),
+    
+    path('myservice', views.portfolio, name='portfolio'),
     
   
 ]
--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -7,3 +7,35 @@ from .models import *
 def index(request):
    
     return render(request, 'myapp/index.html')
+
+def about(request):
+   
+    return render(request, 'myapp/about.html')
+
+def contact(request):
+   
+    return render(request, 'myapp/contact.html')
+
+
+def donate(request):
+   
+    return render(request, 'myapp/donate.html')
+
+
+
+def error(request):
+   
+    return render(request, 'myapp/error.html')
+
+
+
+def myservice(request):
+   
+    return render(request, 'myapp/myservice.html')
+
+
+def portfolio(request):
+   
+    return render(request, 'myapp/portfolio.html')
+
+
